e26dd0fcfc56270097a7e8a27a5c7f303949dae1
Luiz Renan <110012412+luiznwz@users.noreply.github.com>
2024-07-13 16:42:07 -0300

    feat: added new pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,15 @@
-export default function Project() {
+import { FooterPages } from "@/components/footer-pages";
+
+export default function ContactPage() {
   return (
-    <section>
-      <h1>luiz</h1>
-    </section>
+    <div className="relative">
+      <main className="mt-20 px-6">
+        <div className="max-w-[560px] mx-auto flex flex-col gap-20">
+          <h1>LUIZZZZZZZZZZ</h1>
+        </div>
+      </main>
+      <div className="fixed bottom-0 left-0 right-0 h-16 bg-gradient-to-t from-white to-transparent dark:from-[hsl(210,20%,10%)] pointer-events-none"></div>
+      <FooterPages />
+    </div>
   );
 }
