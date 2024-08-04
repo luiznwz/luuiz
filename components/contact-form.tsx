9b70d33f176dd8e5abeb5203235f16fda56b7410
Luiz Renan <110012412+luiznwz@users.noreply.github.com>
2024-08-04 20:59:40 -0300

    fix: linkedin link

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -134,7 +134,7 @@ export function ContactForm(props: ContactFormProps) {
         <p className="mt-8 text-foreground font-normal text-base leading-[1.65]">
           You can see more of my work on{" "}
           <a
-            href="https://www.linkedin.com"
+            href="https://www.linkedin.com/in/luizrenan"
             className="underline decoration-neutral-500 underline-offset-2 hover:decoration-neutral-400 duration-400 transition"
             target="_blank"
             rel="noopener noreferrer"
@@ -165,63 +165,3 @@ export function ContactForm(props: ContactFormProps) {
     </>
   );
 }
-// {
-//   <div className="space-y-4 max-w-[560px]">
-//     <form
-//       onSubmit={onSubmit} // Ligação do handleSubmit com a função onSubmit
-//       target="_blank"
-//       action="https://formsubmit.co/luizrenan179@gmail.com"
-//       method="POST"
-//       className="space-y-4"
-//     >
-//       <div className="grid grid-cols-2 gap-4">
-//         <div className="space-y-2">
-//           <Label className="text-sm" htmlFor="name">
-//             Name
-//           </Label>
-//           <Input
-//             id="name"
-//             placeholder="Enter your name"
-//             {...register("name", { required: true, maxLength: 100 })}
-//           />
-//           {errors.name && (
-//             <p className="text-red-500 text-sm">Name is required</p>
-//           )}
-//         </div>
-//         <div className="space-y-2">
-//           <Label className="text-sm" htmlFor="email">
-//             Email
-//           </Label>
-//           <Input
-//             id="email"
-//             type="email"
-//             placeholder="Enter your email"
-//             {...register("email", { required: true })}
-//           />
-//           {errors.email && (
-//             <p className="text-red-500 text-sm">Email is required</p>
-//           )}
-//         </div>
-//       </div>
-//       <div className="space-y-2">
-//         <Label className="text-sm" htmlFor="message">
-//           Message
-//         </Label>
-//         <Textarea
-//           id="message"
-//           placeholder="Type your message here."
-//           className="min-h-[120px]"
-//           {...register("message", { required: true, maxLength: 2000 })}
-//         />
-//         {errors.message && (
-//           <p className="text-red-500 text-sm">Message is required</p>
-//         )}
-//       </div>
-//       <div className="flex gap-2">
-//         <Button type="submit" variant="outline" className="btn">
-//           Submit
-//         </Button>
-//       </div>
-//     </form>
-//   </div>;
-// }
