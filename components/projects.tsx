e8883c7c8ed1a1b041af91b2684e03c2d6c79453
luiznwz <luizrenan179@gmail.com>
2024-08-24 16:05:58 -0300

    feat: en translation

--- components/projects.tsx
+++ components/projects.tsx
@@ -34,29 +34,6 @@ export const Projects: React.FC<ProjectsProps> = () => {
           className="flex relative justify-between items-start gap-8 group mb-5 sm:mb-3"
         >
           <div className="grow">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              viewBox="0 0 24 24"
-              fill="none"
-              className="absolute sm:left-[-24px] sm:top-[12px] opacity-0 shadow-none outline-none transition-opacity group-hover:opacity-100 group-focus:opacity-100"
-            >
-              <path
-                d="M10 19.0004L9.82843 19.1719C8.26634 20.734 5.73368 20.734 4.17158 19.1719L3.82843 18.8288C2.26634 17.2667 2.26633 14.734 3.82843 13.1719L7.17158 9.8288C8.73368 8.2667 11.2663 8.2667 12.8284 9.8288L13.1716 10.1719C13.8252 10.8256 14.2053 11.6491 14.312 12.5004"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-              <path
-                d="M9.68799 12.5004C9.79463 13.3516 10.1748 14.1752 10.8284 14.8288L11.1715 15.1719C12.7336 16.734 15.2663 16.734 16.8284 15.1719L20.1715 11.8288C21.7336 10.2667 21.7336 7.73404 20.1715 6.17194L19.8284 5.8288C18.2663 4.2667 15.7336 4.2667 14.1715 5.8288L14 6.00037"
-                stroke="currentColor"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              ></path>
-            </svg>
             <h2 className="text-xl py-2 text-secondary-foreground leading-tight font-medium group-hover:underline group-hover:decoration-dashed group-hover:underline-offset-4 group-hover:decoration-1">
               <LinkPreview url={project.projectUrl}>
                 {project.title}
