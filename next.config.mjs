2cf21d3343d6ef9583d2b7fb2c08d10e3b505bbd
luiznwz <luizrenan179@gmail.com>
2025-03-04 21:20:25 -0300

    fix: image domains deprecated & removing vercel toolbar

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["api.microlink.io"],
+    remotePatterns: ["api.microlink.io"],
   },
 };
 
-import withVercelToolbar from '@vercel/toolbar/plugins/next';
-
-// Aplicando o withVercelToolbar e exportando a configuração
-const config = withVercelToolbar()(nextConfig);
 
 export default config;
